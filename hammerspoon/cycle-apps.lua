8de30820ab1ac6f7f2a575acf049738e7d290be7
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2022-01-01 17:18:54 +0100

    focus window and not just app

--- hammerspoon/cycle-apps.lua
+++ hammerspoon/cycle-apps.lua
@@ -1,12 +1,27 @@
-local apps = { "VSCodium" }
+local apps = {
+  {
+    ["name"]= "Code - Insiders",
+    ["titlePattern"] = "Dendron"
+  }
+}
 local appsCount = #apps
 local currentIdx = nil
+-- local log = hs.logger.new('cycle-apps.lua', 'debug')
+
 
 -- if app of index i is running, pass it to given fn (otherwise skip)
 local withApp = function(i, fn)
-  local app = hs.application.get(apps[i])
+  local app = hs.application.get(apps[i]["name"])
   if app then
-    return fn(app, i)
+    local titlePattern = apps[i]["titlePattern"]
+    if titlePattern then
+      local win = app:findWindow(titlePattern)
+      if win then
+        return fn(app, win, i)
+      end
+    else
+      return fn(app, app:mainWindow(), i)
+    end
   end
   return false
 end
@@ -23,16 +38,16 @@ local tryApps = function(startIdx, fn)
 end
 
 local findAndShowNextOpenApp = function(startIdx)
-  tryApps(startIdx, function(app, i)
-    app:activate()
+  tryApps(startIdx, function(app, win, i)
+    win:focus()
     currentIdx = i
     return true
   end)
 end
 
 -- find current app on load, if any
-tryApps(1, function(app, i)
-  if app:isFrontmost() then
+tryApps(1, function(app, win, i)
+  if app:isFrontmost() and app:focusedWindow() == win then
     currentIdx = i
     return true
   end
@@ -46,16 +61,16 @@ hs.hotkey.bind({"cmd", "shift"}, "space", function()
       currentIdx = currentIdx + 1
       findAndShowNextOpenApp(currentIdx)
     else
-      local didActivateCurrent = withApp(currentIdx, function(app)
-        if not app:isFrontmost() then
-          app:activate()
+      local didActivateCurrent = withApp(currentIdx, function(app, win)
+        if not (app:isFrontmost() and app:focusedWindow() == win) then
+          win:focus()
           return true
         end
       end)
       if not didActivateCurrent then
         currentIdx = nil
         for i = 1, appsCount do
-          withApp(i, function(app)
+          withApp(i, function(app, win)
             app:hide()
           end)
         end
