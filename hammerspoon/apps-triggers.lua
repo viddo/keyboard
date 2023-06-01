b98f867e802c53aecad416c3561c4377b2d96663
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2023-06-01 21:39:27 +0200

    Start/stop app depending on Zoom

---
+++ hammerspoon/apps-triggers.lua
@@ -0,0 +1,20 @@
+-- log = hs.logger.new('apps-trigger.lua', 'debug')
+
+hd = "Hands Down"
+
+watcher = hs.application.watcher.new(function(appName, eventType, app)
+  -- log.d('appName', appName)
+  -- log.d('evenType', eventType)
+  if appName == "zoom.us" then
+    if eventType == hs.application.watcher.launched then
+      --- stop
+      local hdApp = hs.application.get(hd)
+      if hdApp then
+        hdApp:kill()
+      end
+    elseif eventType == hs.application.watcher.terminated then
+      hs.application.launchOrFocus(hd)
+    end
+  end
+end)
+watcher:start()
