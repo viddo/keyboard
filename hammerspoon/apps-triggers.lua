824c54cc68cd56933b533e8f4be327e86e90e3b1
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2023-11-28 16:53:07 +0100

    hd tweaks

--- hammerspoon/apps-triggers.lua
+++ hammerspoon/apps-triggers.lua
@@ -2,19 +2,31 @@
 
 hd = "Hands Down"
 
+function stopHandsDown()
+  local hdApp = hs.application.get(hd)
+  if hdApp then
+    hdApp:kill()
+  end
+end
+
+function startHandsDown()
+  hs.application.launchOrFocus(hd)
+end
+
 watcher = hs.application.watcher.new(function(appName, eventType, app)
   -- log.d('appName', appName)
   -- log.d('evenType', eventType)
   if appName == "zoom.us" or appName == 'FaceTime' then
     if eventType == hs.application.watcher.launched then
-      --- stop
-      local hdApp = hs.application.get(hd)
-      if hdApp then
-        hdApp:kill()
-      end
+      stopHandsDown()
     elseif eventType == hs.application.watcher.terminated then
-      hs.application.launchOrFocus(hd)
-    end
+      startHandsDown()
+    end 
   end
 end)
 watcher:start()
+
+local wf=hs.window.filter
+wf_slack = wf.new(false):setAppFilter('Slack', { allowTitles = 'Huddle' }) -- Any Slack windows with "huddle" anywhere in the title
+wf_slack:subscribe(wf.hasWindow, stopHandsDown)
+wf_slack:subscribe(wf.hasNoWindows, startHandsDown)
